0e0684c093415d93453876211210fcac3636323a
Andrew Hopkins <andhop@amazon.com>
2023-12-03 15:53:04 -0800

    Cleanup day 1 part 1

--- 2023/day-01/asm.asm
+++ 2023/day-01/asm.asm
@@ -1,76 +1,76 @@
 #define NULL 0
-#define NEWLINE #10
+#define newline #10
 #define ASCII_0 #48
 #define ASCII_9 #57
-#define FIRST_FOUND #1
+#define first_flag #1
 
 // Function parameters
-#define STRING_PTR x0
+#define string_ptr x0
 
 // Algorithm variables
-#define TEMP x1
-#define SUM x2
-#define FIRST x3
-#define LAST x4
-#define FLAGS x5
-#define CURRENT_BYTE w6
-#define CURRENT x6
+#define temp x1
+#define sum x2
+#define first x3
+#define last x4
+#define flags x5
+#define current_byte w6
+#define current x6
 
 
 .globl	_puzzle1
 .p2align   4
 _puzzle1:
     // Prologue
-    stp x29, x30, [sp, #-16]!  ; store pair x29 and x30 to the stack and decrement the stack pointer
+    stp x29, x30, [sp, #-16]!
     mov x29, sp
 
     // Setup
-    mov SUM, #0
-    mov FLAGS, #0
+    mov sum, #0
+    mov flags, #0
 
 loop:
-    ldrb CURRENT_BYTE, [STRING_PTR]
-    cmp CURRENT, NULL
+    ldrb current_byte, [string_ptr]
+    cmp current, NULL
     b.eq end_loop
 
-    cmp CURRENT, NEWLINE
+    cmp current, newline
     b.ne search
     // This is a newline run the calculation for the previous line
-    mov TEMP, #10
-    mul FIRST, FIRST, TEMP
-    add SUM, SUM, FIRST
-    add SUM, SUM, LAST
-    mov FLAGS, #0
+    mov temp, #10
+    mul first, first, temp
+    add sum, sum, first
+    add sum, sum, last
+    mov flags, #0
     b next
 
 search:
-    cmp CURRENT, ASCII_0
+    cmp current, ASCII_0
     b.lt next
 
-    cmp CURRENT, ASCII_9
+    cmp current, ASCII_9
     b.gt next
 
     // This is an ascii number get the actual value
-    sub CURRENT, CURRENT, ASCII_0
+    sub current, current, ASCII_0
 
-    and TEMP, FLAGS, FIRST_FOUND
-    cmp TEMP, 0
+    and temp, flags, first_flag
+    cmp temp, 0
     b.gt found_last
-    mov FIRST, CURRENT
-    mov LAST, CURRENT
-    orr FLAGS, FLAGS, FIRST_FOUND
+    mov first, current
+    mov last, current
+    orr flags, flags, first_flag
     b next
 
 found_last:
-    mov LAST, CURRENT
+    mov last, current
     b next
 
 next:
-    add STRING_PTR, STRING_PTR, #1
+    add string_ptr, string_ptr, #1
     b loop
 
 end_loop:
-    mov x0, SUM
+    mov x0, sum
 
     // Epilogue
     ldp x29, x30, [sp], #16  ; load pair x29 and x30 from the stack and increment the stack pointer
