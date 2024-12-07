56fcc3a9a363c9ff488fefb9acd939e1bdd6ec26
Andrew Hopkins <andhop@amazon.com>
2024-12-06 16:55:54 -0800

    CLeanup day 3 and share regex

--- 2024-rust/src/day3.rs
+++ 2024-rust/src/day3.rs
@@ -1,7 +1,7 @@
 use once_cell::sync::Lazy;
 use regex::Regex;
 
-fn day3_regex(input: &Vec<u8>, regex: &Regex) -> u32 {
+fn day3_part1_regex_core(input: &Vec<u8>, regex: &Regex) -> u32 {
     let ascii_str = std::str::from_utf8(input).expect("input was not UTF8 string");
     let result = regex
         .captures_iter(ascii_str)
@@ -14,16 +14,17 @@ fn day3_regex(input: &Vec<u8>, regex: &Regex) -> u32 {
     result
 }
 
+const PART1_REGEX_STRING: &str = r"mul\((?<left>\d{1,3}),(?<right>\d{1,3})\)";
+
 pub fn day3_part1_regex(input: &Vec<u8>) -> u32 {
-    let mul_regex = Regex::new(r"mul\((?<left>\d{1,3}),(?<right>\d{1,3})\)").unwrap();
-    day3_regex(input, &mul_regex)
+    let mul_regex = Regex::new(PART1_REGEX_STRING).unwrap();
+    day3_part1_regex_core(input, &mul_regex)
 }
 
-static MUL_REGEX: Lazy<Regex> =
-    Lazy::new(|| Regex::new(r"mul\((?<left>\d{1,3}),(?<right>\d{1,3})\)").unwrap());
+static MUL_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(PART1_REGEX_STRING).unwrap());
 
 pub fn day3_part1_lazy_regex(input: &Vec<u8>) -> u32 {
-    day3_regex(input, &*MUL_REGEX)
+    day3_part1_regex_core(input, &*MUL_REGEX)
 }
 
 #[cfg(test)]
